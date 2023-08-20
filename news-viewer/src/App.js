5e5cbb4baf99a8ad816c29b93d8f98e406c1d492
ujujungbuck <garam8u2@gmail.com>
2023-08-20 17:36:02 +0900

    Feat : api연결

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,28 +1,13 @@
 import React, { useState } from "react";
-import axios from "axios";
+import NewsList from "./components/NewsList";
+import Categories from "./components/Categories";
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = () => {
-    axios
-      .get("http://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => {
-        setData(response.data);
-      });
-  };
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
+    <>
+      <Categories />
+      <NewsList />
+    </>
   );
 };
 
---
+++ news-viewer/src/components/NewsList.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import NewsItem from "./NewsItem";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const sampleArticle = {
+  title: "title",
+  description: "content blah~~~",
+  url: "naver.com",
+  urlToImage: "https://via.placeholder.com/160",
+};
+
+const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    //async를 사용하는 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "https://newsapi.org/v2/top-headlines?country=kr&category=business&apiKey=7881637c22d3457a87325e85b9ef5173"
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <NewsListBlock>waiting...!</NewsListBlock>;
+  }
+
+  if (!articles) {
+    return null;
+  }
+
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
