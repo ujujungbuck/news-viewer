1dccdc6bb1d561eeca29f5a1e8f6c753c0d596c5
ujujungbuck <garam8u2@gmail.com>
2023-08-21 16:27:38 +0900

    Feat : usePromise추가

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import NewsItem from "./NewsItem";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import usePromise from "../lib/usePromise";
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -23,35 +24,26 @@ const sampleArticle = {
   urlToImage: "https://via.placeholder.com/160",
 };
 
-const NewsList = () => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    //async를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get(
-          "https://newsapi.org/v2/top-headlines?country=kr&category=business&apiKey=7881637c22d3457a87325e85b9ef5173"
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
+const NewsList = ({ category }) => {
+  const [loading, response, error] = usePromise(() => {
+    const query = category === "all" ? "" : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=7881637c22d3457a87325e85b9ef5173`
+    );
+  }, [category]);
 
   if (loading) {
-    return <NewsListBlock>waiting...!</NewsListBlock>;
+    return <NewsListBlock>로딩중입니다!</NewsListBlock>;
   }
 
-  if (!articles) {
+  if (!response) {
     return null;
   }
 
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
